505e1c3e2fed01620548354274bf128f1c78ad50
Northword <northword@outlook.com>
2025-08-03 11:29:51 +0800

    chore: bump node-style-text to v1.0.0

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    projects: [
-      "src",
-      "test",
-    ],
+    deps: {
+      // https://github.com/fisker/node-style-text/issues/27
+      interopDefault: false,
+    },
   },
 },
 );
