72c92dee25871f6dda58f6d4346e2fe55d1dde06
Northword <northword@outlook.com>
2025-11-21 20:14:18 +0800

    bundle chengelogen dts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/index.ts", "./src/vendor", "./src/cli.ts"],
-  dts: true,
   clean: true,
+  unbundle: true,
   noExternal: ["node-style-text", "changelogen"],
+  dts: {
+    resolve: ["changelogen"],
+  },
 });
