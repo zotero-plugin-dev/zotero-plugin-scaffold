16e65cbfd2495b730bbe35a8c2936ee87ed18e3f
Northword <northword@outlook.com>
2025-11-21 20:28:27 +0800

    remove node-style-text

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts", "./src/vendor", "./src/cli.ts"],
   clean: true,
-  unbundle: true,
-  noExternal: ["node-style-text", "changelogen"],
+  // unbundle: true,
+  noExternal: ["changelogen"],
   dts: {
     resolve: ["changelogen"],
   },
