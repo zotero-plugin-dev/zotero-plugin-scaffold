45a1c45cc82125163daba391eb647df9b8efdd57
Northword <northword@outlook.com>
2025-11-22 16:15:24 +0800

    chore: use tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,28 @@
+import Module from "node:module";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/index.ts", "./src/vendor", "./src/cli.ts"],
   clean: true,
   // unbundle: true,
+
+  // Since we bundled changelogen -> node-fetch-native,
+  // we expect these patches to reduce the bundle size.
+  // https://github.com/rolldown/tsdown/issues/611
+  platform: "neutral",
+  external: [
+    ...Module.builtinModules,
+    ...Module.builtinModules.map(m => `node:${m}`),
+  ],
   noExternal: ["changelogen"],
   dts: {
     resolve: ["changelogen"],
+  },
+
+  // I think the default chunk naming in unbuild is very elegant,
+  // so we config for it in tsdown.
+  // https://github.com/rolldown/tsdown/discussions/612
+  outputOptions: {
+    chunkFileNames: "shared/scaffold-[name]-[hash].mjs",
   },
 });
