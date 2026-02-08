a6119d6228fe12214dff0bc3a2c9f6bc8cb8d7e3
Northword <northword@outlook.com>
2026-02-08 17:25:50 +0800

    refactor: migrate swc to oxc

--- test/e2e/snap/dist/addon/prefs.js
+++ test/e2e/snap/dist/addon/prefs.js
@@ -3,5 +3,5 @@ pref("extensions.testplugin.boolean", true);
 pref("extensions.testplugin.number", 123);
 pref("extensions.testplugin.break-line", "123");
 pref("extensions.testplugin.fake-number", "123");
-pref("extensions.testplugin.templateLiteral", '{\n  "key": "value"  \n}');
+pref("extensions.testplugin.templateLiteral", "{\n  \"key\": \"value\"  \n}");
 pref("extensions.testplugin.long-number", 2222222222222222);
