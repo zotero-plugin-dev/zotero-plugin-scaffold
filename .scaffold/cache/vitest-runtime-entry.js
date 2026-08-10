c1d9304b940df5b3ca3d8d5874ed080c05c8d274
Northword <northword@outlook.com>
2026-08-10 16:02:32 +0800

    chore: ignore .scaffold cache directory

--- .scaffold/cache/vitest-runtime-entry.js
+++
@@ -1,7 +0,0 @@
-
-export { expect, vi, assert, should } from "vitest";
-export {
-  describe, it, test, suite,
-  beforeAll, afterAll, beforeEach, afterEach,
-  startTests,
-} from "@vitest/runner";
